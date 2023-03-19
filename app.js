868cb6bf359b40df2a1fb91438b075ff69c0f8b8
brickphone <mattis.hutt@gmail.com>
2023-03-19 15:14:57 +0100

    final

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const container = document.getElementById("container")
-const gridSize = 16;
+let gridSize = 16;
 
 createGrid();
 
@@ -20,3 +20,23 @@ function restartPlay() {
   container.innerHTML = "";
   createGrid();
 }
+
+const sizeButton = document.querySelector(".size-selector");
+
+sizeButton.addEventListener("click", changeGridSize);
+
+function changeGridSize() {
+  let newSize = prompt("Enter a size from 1-64");
+  if (newSize === null) {
+    return;
+  }
+  newSize = parseInt(newSize);
+  if (isNaN(newSize) || newSize < 1 || newSize > 64) {
+    alert("Invalid number selection! please try again");
+    changeGridSize();
+  } else {
+    gridSize = newSize;
+    container.innerHTML = "";
+    createGrid();
+  }
+};
